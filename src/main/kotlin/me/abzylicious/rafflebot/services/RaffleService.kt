a6822e94881adf9b694f2fca04c2e6ca5eae0fa5
Alexander Deledda <Alexander.Deledda@gmail.com>
2020-07-20 21:30:14 +0200

    Add resolve functionality to RaffleService

--- src/main/kotlin/me/abzylicious/rafflebot/services/RaffleService.kt
+++ src/main/kotlin/me/abzylicious/rafflebot/services/RaffleService.kt
@@ -1,15 +1,26 @@
 package me.abzylicious.rafflebot.services
 
+import me.abzylicious.rafflebot.extensions.jda.getMessageById
+import me.abzylicious.rafflebot.extensions.jda.getReaction
+import me.abzylicious.rafflebot.extensions.jda.getUsers
+import me.abzylicious.rafflebot.extensions.stdlib.toChannel
 import me.abzylicious.rafflebot.persistence.Raffle
 import me.abzylicious.rafflebot.persistence.RaffleRepository
+import me.abzylicious.rafflebot.utilities.Randomizer
 import me.jakejmattson.kutils.api.Discord
 import me.jakejmattson.kutils.api.annotations.Service
+import me.jakejmattson.kutils.api.extensions.jda.fullName
 import me.jakejmattson.kutils.api.services.PersistenceService
+import net.dv8tion.jda.api.entities.User
+
+data class Winner(val id: String, val name: String)
 
 @Service
 class RaffleService(private val discord: Discord,
                     private val persistenceService: PersistenceService,
-                    private val repository: RaffleRepository = RaffleRepository(discord, persistenceService)) {
+                    private val repository: RaffleRepository = RaffleRepository(discord, persistenceService),
+                    private val randomizer: Randomizer<User>
+) {
 
     fun addRaffle(channelId: String, messageId: String, reaction: String): Boolean {
         val exists = repository.exists(messageId)
@@ -19,5 +30,21 @@ class RaffleService(private val discord: Discord,
         return !exists
     }
 
+    fun resolveRaffle(messageId: String, winnerCount: Int = 1): List<Winner> {
+        val exists = repository.exists(messageId)
+        if (!exists)
+            return listOf()
 
+        val raffle = repository.get(messageId)!!
+        val participants = getRaffleParticipants(raffle).filter { !it.isBot }
+        val winnerPool = randomizer.selectRandom(participants, winnerCount)
+        return winnerPool.map { Winner(it.id, it.name) }
+    }
+
+    private fun getRaffleParticipants(raffle: Raffle): List<User> {
+        val channel = raffle.ChannelId.toChannel() ?: return emptyList()
+        val message = channel.getMessageById(raffle.MessageId)
+        val reaction = message.getReaction(raffle.Reaction)
+        return reaction.getUsers()
+    }
 }
