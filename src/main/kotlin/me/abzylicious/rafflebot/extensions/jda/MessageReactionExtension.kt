9c6e1b379f4ec60df40b63b9fac8025975e072c9
Alexander Deledda <Alexander.Deledda@gmail.com>
2020-11-01 23:12:27 +0100

    delete jda extensions

--- src/main/kotlin/me/abzylicious/rafflebot/extensions/jda/MessageReactionExtension.kt
+++
@@ -1,6 +0,0 @@
-package me.abzylicious.rafflebot.extensions.jda
-
-import net.dv8tion.jda.api.entities.MessageReaction
-import net.dv8tion.jda.api.entities.User
-
-fun MessageReaction.getUsers(): List<User> = this.retrieveUsers().complete()
--- src/main/kotlin/me/abzylicious/rafflebot/extensions/jda/TextChannelExtension.kt
+++
@@ -1,6 +0,0 @@
-package me.abzylicious.rafflebot.extensions.jda
-
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.TextChannel
-
-fun TextChannel.getMessageById(messageId: String): Message = this.retrieveMessageById(messageId).complete()
