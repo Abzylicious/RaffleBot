08e1e9245739710790b88fbd775568965a12b0f6
Alexander Deledda <Alexander.Deledda@gmail.com>
2020-11-22 21:17:00 +0100

    add BotInfoEmbeds
    * adjust Main

--- src/main/kotlin/me/abzylicious/rafflebot/Main.kt
+++ src/main/kotlin/me/abzylicious/rafflebot/Main.kt
@@ -4,13 +4,11 @@ import com.gitlab.kordlib.gateway.Intent
 import com.google.gson.Gson
 import me.abzylicious.rafflebot.configuration.Configuration
 import me.abzylicious.rafflebot.configuration.Messages
-import me.abzylicious.rafflebot.services.BotStatsService
+import me.abzylicious.rafflebot.embeds.Project
+import me.abzylicious.rafflebot.embeds.createBotInformationEmbed
 import me.abzylicious.rafflebot.services.LoggingService
 import me.jakejmattson.discordkt.api.dsl.bot
-import me.jakejmattson.discordkt.api.extensions.addInlineField
 import java.awt.Color
-
-data class Properties(val author: String, val version: String, val discordkt: String, val kotlin: String, val repository: String)
 
 suspend fun main(args: Array<String>) {
     val messages = Messages()
@@ -18,8 +16,8 @@ suspend fun main(args: Array<String>) {
 
     require(token != null) { messages.NO_TOKEN_PROVIDED }
 
-    val propertyFile = Properties::class.java.getResource("/properties.json").readText()
-    val project = Gson().fromJson(propertyFile, Properties::class.java)
+    val propertyFile = Project::class.java.getResource("/properties.json").readText()
+    val project = Gson().fromJson(propertyFile, Project::class.java)
 
     bot(token) {
         prefix {
@@ -36,30 +34,7 @@ suspend fun main(args: Array<String>) {
         }
 
         mentionEmbed {
-            val botStats = it.discord.getInjectionObjects(BotStatsService::class)
-            val self = it.discord.api.getSelf()
-
-            color = it.discord.configuration.theme
-            thumbnail { url = self.avatar.url }
-
-            title = self.tag
-            description = "A multi-guild discord bot to host all the giveaways you could ever want"
-
-            addInlineField("Author", project.author)
-            addInlineField("Source", "[GitHub](${project.repository})")
-            addInlineField("Prefix", it.prefix())
-
-            field {
-                name = "Build Info"
-                value = "```" +
-                        "Version: ${project.version}\n" +
-                        "DiscordKt: ${project.discordkt}\n" +
-                        "Kotlin: ${project.kotlin}\n" +
-                        "```"
-            }
-
-            addInlineField("Uptime", botStats.uptime)
-            addInlineField("Ping", botStats.ping)
+            createBotInformationEmbed(it, project)
         }
 
         onStart {
